95a9902abb8e889cf92579937d652ff70a731916
Ludovico96 <spadaro.ludovico@gmail.com>
2024-01-06 12:05:35 +0100

    add registration function

--- gameHub/src/main/java/it/unipi/lsmsd/gamehub/service/ILoginService.java
+++ gameHub/src/main/java/it/unipi/lsmsd/gamehub/service/ILoginService.java
@@ -1,7 +1,9 @@
 package it.unipi.lsmsd.gamehub.service;
 
 import it.unipi.lsmsd.gamehub.DTO.LoginDTO;
+import it.unipi.lsmsd.gamehub.DTO.RegistrationDTO;
 
 public interface ILoginService {
     public boolean authenticate(LoginDTO loginDTO);
+    public boolean registrate(RegistrationDTO registrationDTO);
 }
