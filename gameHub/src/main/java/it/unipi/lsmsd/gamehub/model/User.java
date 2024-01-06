6f685f00d8d5f8d25895dfadf14c26c5a329a112
Ludovico96 <spadaro.ludovico@gmail.com>
2024-01-06 11:57:19 +0100

    adding null constructor

--- gameHub/src/main/java/it/unipi/lsmsd/gamehub/model/User.java
+++ gameHub/src/main/java/it/unipi/lsmsd/gamehub/model/User.java
@@ -1,18 +1,17 @@
 package it.unipi.lsmsd.gamehub.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 @ToString
 @Document(collection = "users")
 public class User {
+
     @Id
     String id;
     String username;
@@ -20,4 +19,6 @@ public class User {
     String surname;
     String password;
     String email;
+
+
 }
