6f5945b938f0a7894b53e0bf87979e787762c310
Ludovico96 <spadaro.ludovico@gmail.com>
2024-01-08 13:43:59 +0100

    Url model

---
+++ gameHub/src/main/java/it/unipi/lsmsd/gamehub/model/URL.java
@@ -0,0 +1,17 @@
+package it.unipi.lsmsd.gamehub.model;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class URL {
+    private String Website;
+    private String Header_image;
+    private String Support_url;
+    private String Support_email;
+    private String Screenshots;
+
+}
