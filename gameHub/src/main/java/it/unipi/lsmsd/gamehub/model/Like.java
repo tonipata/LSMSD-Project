3d6ccc90caca5e84ee7ea9256ff9638ed9d855e4
Ludovico96 <spadaro.ludovico@gmail.com>
2024-01-15 19:53:11 +0100

    modified like

--- gameHub/src/main/java/it/unipi/lsmsd/gamehub/model/Like.java
+++ gameHub/src/main/java/it/unipi/lsmsd/gamehub/model/Like.java
@@ -1,8 +1,11 @@
 package it.unipi.lsmsd.gamehub.model;
 
 import lombok.*;
+import org.springframework.data.annotation.PersistenceConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @AllArgsConstructor
@@ -10,8 +13,10 @@ import java.util.List;
 @Getter
 @Setter
 @ToString
-
+@Document(collection = "games")
 public class Like {
+    @Field("count")
     private int count;
-    private List<String> usersID;
+    @Field("user_like")
+    private List<String> userLike;
 }
