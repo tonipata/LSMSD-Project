4d39c67e5fcbe82a8cba05a4af039ac30e1e9348
Ludovico96 <spadaro.ludovico@gmail.com>
2024-01-15 19:51:21 +0100

    add function controller

--- gameHub/src/main/java/it/unipi/lsmsd/gamehub/controller/GameController.java
+++ gameHub/src/main/java/it/unipi/lsmsd/gamehub/controller/GameController.java
@@ -1,9 +1,12 @@
 package it.unipi.lsmsd.gamehub.controller;
 
 import it.unipi.lsmsd.gamehub.DTO.GameDTO;
+import it.unipi.lsmsd.gamehub.DTO.GameDTOAggregation;
+import it.unipi.lsmsd.gamehub.DTO.GameDTOAggregation2;
 import it.unipi.lsmsd.gamehub.model.Game;
 import it.unipi.lsmsd.gamehub.service.IGameService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.aggregation.AggregationResults;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -24,6 +27,29 @@ public class GameController {
         System.out.println("gamelist empty");
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
     }
+
+    @GetMapping("/prova")
+    public ResponseEntity<List<GameDTOAggregation>> retrieveAggregateGamesByGenresAndSortByScore() {
+        List<GameDTOAggregation> gameList = gameService.retrieveAggregateGamesByGenresAndSortByScore();
+
+        if (!gameList.isEmpty()) {
+            return ResponseEntity.ok(gameList);
+        }
+        System.out.println("gamelist empty");
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+    }
+
+    @GetMapping("/prova4")
+    public ResponseEntity<List<GameDTOAggregation2>> findAggregation() {
+        List<GameDTOAggregation2> gameList = gameService.findAggregation4();
+
+        if (!gameList.isEmpty()) {
+            return ResponseEntity.ok(gameList);
+        }
+        System.out.println("gamelist empty");
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+    }
+
 
 }
 
