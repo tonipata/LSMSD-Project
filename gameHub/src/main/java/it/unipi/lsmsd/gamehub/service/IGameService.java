e11f6092d151967e43e991c65c573c285d245de7
Ludovico96 <spadaro.ludovico@gmail.com>
2024-01-15 19:56:04 +0100

    modified IGameService

--- gameHub/src/main/java/it/unipi/lsmsd/gamehub/service/IGameService.java
+++ gameHub/src/main/java/it/unipi/lsmsd/gamehub/service/IGameService.java
@@ -1,14 +1,22 @@
 package it.unipi.lsmsd.gamehub.service;
 
 import it.unipi.lsmsd.gamehub.DTO.GameDTO;
+import it.unipi.lsmsd.gamehub.DTO.GameDTOAggregation;
+import it.unipi.lsmsd.gamehub.DTO.GameDTOAggregation2;
 import it.unipi.lsmsd.gamehub.model.Game;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.core.aggregation.AggregationResults;
+
 
 import java.util.List;
 
 public interface IGameService {
     public List<Game> retrieveGamesByParameters(GameDTO gameDTO);
 
+    public List<GameDTOAggregation> retrieveAggregateGamesByGenresAndSortByScore();
 
+    public List<GameDTOAggregation2> findAggregation4();
 
 
 }
