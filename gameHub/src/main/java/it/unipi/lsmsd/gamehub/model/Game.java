187d75c76af962d826f3463ab0c85991939c140b
Ludovico96 <spadaro.ludovico@gmail.com>
2024-01-15 19:52:16 +0100

    modified Game

--- gameHub/src/main/java/it/unipi/lsmsd/gamehub/model/Game.java
+++ gameHub/src/main/java/it/unipi/lsmsd/gamehub/model/Game.java
@@ -1,8 +1,14 @@
 package it.unipi.lsmsd.gamehub.model;
 
 import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.index.CompoundIndexes;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
+import java.util.ArrayList;
 import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
@@ -10,20 +16,38 @@ import java.util.List;
 @Setter
 @ToString
 @Document(collection = "games")
+@CompoundIndexes({
+        @CompoundIndex(name = "genres_avgScore", def = "{'genres' : 1, 'avgScore': 1}")
+})
 
 public class Game {
+    @Id
     private String id;
+    @Field("name")
     private String name;
+    @Indexed
+    @Field("genres")
     private String genres;
+    @Field("releaseDate")
     private String releaseDate;
+    @Indexed
+    @Field("avgScore")
     private int avgScore;
-    private double Price;
-    private String About_the_game;
-    private String Supported_languages;
-    private String Developers;
-    private String Publishers;
-    private String Categories;
+    @Field("Price")
+    private double price;
+    @Field("About the game")
+    private String aboutTheGame;
+    @Field("Supported languages")
+    private String supportedLanguages;
+    @Field("Developers")
+    private String developers;
+    @Field("Publishers")
+    private String publishers;
+    @Field("Categories")
+    private String categories;
+    @Field("URL")
     private URL URL;
-    private List<Review> Reviews;
+    @Field("Reviews")
+    private List<Review> reviews;
 
 }
