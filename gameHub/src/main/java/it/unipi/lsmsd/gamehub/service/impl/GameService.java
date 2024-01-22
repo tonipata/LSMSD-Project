65e061a4b30b8a7ee7458a6805d5095e4a1fbc73
Ludovico96 <spadaro.ludovico@gmail.com>
2024-01-22 13:16:05 +0100

    add getAll

--- gameHub/src/main/java/it/unipi/lsmsd/gamehub/service/impl/GameService.java
+++ gameHub/src/main/java/it/unipi/lsmsd/gamehub/service/impl/GameService.java
@@ -7,9 +7,14 @@ import it.unipi.lsmsd.gamehub.model.Game;
 import it.unipi.lsmsd.gamehub.repository.GameRepository;
 import it.unipi.lsmsd.gamehub.repository.MongoDBAggregation.GameRepositoryCustom;
 import it.unipi.lsmsd.gamehub.service.IGameService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -66,6 +71,19 @@ public class GameService implements IGameService {
         }catch (Exception e){
             System.out.println(e.getMessage());
             return null;
+        }
+    }
+
+    @Override
+    public Page<Game> getAll(Pageable pageable) {
+        try {
+            Page<Game> games =  gameRepository.findAll(pageable);
+            ModelMapper modelMapper = new ModelMapper();
+            return games.map(game -> modelMapper.map(game, Game.class));
+        }
+        catch (Exception e) {
+            System.out.println("Errore durante il recupero dei giochi: " + e.getMessage());
+            return new PageImpl<>(Collections.emptyList(), pageable, 0);
         }
     }
 
