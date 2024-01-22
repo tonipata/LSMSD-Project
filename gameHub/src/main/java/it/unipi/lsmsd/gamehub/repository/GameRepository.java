101acdb7035831adec6adbdc22bed14e07862853
Ludovico96 <spadaro.ludovico@gmail.com>
2024-01-22 12:49:34 +0100

    modified gameRepository

--- gameHub/src/main/java/it/unipi/lsmsd/gamehub/repository/GameRepository.java
+++ gameHub/src/main/java/it/unipi/lsmsd/gamehub/repository/GameRepository.java
@@ -2,6 +2,8 @@ package it.unipi.lsmsd.gamehub.repository;
 
 import it.unipi.lsmsd.gamehub.model.Game;
 import it.unipi.lsmsd.gamehub.repository.MongoDBAggregation.GameRepositoryCustom;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -14,6 +16,7 @@ public interface GameRepository extends MongoRepository<Game,String>, GameReposi
     List<Game> findByGenres(String genres);
     List<Game> findByAvgScoreGreaterThanEqual(int avgScore);
     List<Game> findByGenresAndAvgScoreGreaterThanEqual(String genres,int avgScore);
+    Page<Game> findAll(Pageable pageable);
 
 
 
