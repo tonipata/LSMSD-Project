270a964df7044115b903b6b791ad9d0f9f4c287a
Ludovico96 <spadaro.ludovico@gmail.com>
2024-01-22 12:52:20 +0100

    add gameNeo4jCOntroller

---
+++ gameHub/src/main/java/it/unipi/lsmsd/gamehub/controller/GameNeo4jController.java
@@ -0,0 +1,57 @@
+package it.unipi.lsmsd.gamehub.controller;
+
+import it.unipi.lsmsd.gamehub.DTO.GameDTO;
+import it.unipi.lsmsd.gamehub.model.Game;
+import it.unipi.lsmsd.gamehub.model.GameNeo4j;
+import it.unipi.lsmsd.gamehub.model.UserNeo4j;
+import it.unipi.lsmsd.gamehub.service.impl.GameNeo4jService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.aggregation.ArrayOperators;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class GameNeo4jController {
+
+    @Autowired
+    GameNeo4jService gameNeo4jService;
+    /*@GetMapping("/wishlist")
+    public ResponseEntity<List<GameNeo4j>> getUSerWishlist(@RequestBody String username) {
+        List<GameNeo4j> gameList = gameNeo4jService.getUserWishlist(username);
+        if (!gameList.isEmpty()) {
+            return ResponseEntity.ok(gameList);
+        }
+        System.out.println("gamelist empty");
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+    }*/
+
+
+
+    @GetMapping("/provaNeo")
+    public ResponseEntity<GameNeo4j> getGameByName(@RequestParam String name) {
+        GameNeo4j gameList = gameNeo4jService.getGameByTitle(name);
+        if (gameList!=null) {
+            return ResponseEntity.ok(gameList);
+        }
+        System.out.println("gamelist empty");
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+    }
+
+    @GetMapping("/provaNeo2")
+    public ResponseEntity<Integer> getGamesIngoingLinks(@RequestParam String name) {
+       Integer countLinks= gameNeo4jService.getGamesIngoingLinks(name);
+        if (countLinks!=null) {
+            return ResponseEntity.ok(countLinks);
+        }
+        System.out.println("gamelist empty");
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+    }
+
+
+}
