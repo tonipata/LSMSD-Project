187c8f8b68fad7d46235e7a313813e85392b7213
Ludovico96 <spadaro.ludovico@gmail.com>
2024-01-22 12:55:26 +0100

    add gameNeo4jRepository

---
+++ gameHub/src/main/java/it/unipi/lsmsd/gamehub/repository/GameNeo4jRepository.java
@@ -0,0 +1,27 @@
+package it.unipi.lsmsd.gamehub.repository;
+
+
+import it.unipi.lsmsd.gamehub.model.GameNeo4j;
+import org.springframework.data.neo4j.repository.query.Query;
+import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.neo4j.core.Neo4jOperations;
+
+import java.util.List;
+
+@Repository
+public interface GameNeo4jRepository extends Neo4jRepository<GameNeo4j, String> {
+  @Query("MATCH (g:GameNeo4j) WHERE g.name = $name RETURN g ")
+   GameNeo4j findGameByName(@Param("name") String name);
+
+
+   //@Query("MATCH (g:GameNeo4j)<-[:ADD]-(u:UserNeo4j) RETURN g.id as id, g.name as name, g.developers as developers, g.categories as categories, g.genres as genres, count(u) as numberOfLinks ORDER BY numberOfLinks DESC LIMIT 10")
+   @Query("MATCH (g:GameNeo4j)<-[:ADD]-(u:UserNeo4j) WHERE g.name = $name RETURN count(u) as numberOfLinks")
+    int findGameIngoingLinks(@Param("name") String name);
+
+
+
+
+
+}
